c1ff523492422c4d87d910931691e73e0bac5fcf
barrown <78792177+barrown@users.noreply.github.com>
2024-05-24 11:36:49 +0100

    first commit

---
+++ emf-camp-schedule-viewer.py
@@ -0,0 +1,62 @@
+from json import loads
+from datetime import datetime
+from requests import get
+
+# paste in your personal URL
+your_url_here = "https://www.emfcamp.org/favourites.json?token=248-yet_PvW9g2ezfSNgcePG"
+emftalks = loads(get(your_url_here).text)
+
+# or if you've downloaded your file, point to it here
+#emftalks = json.loads(open("emftalks.json", 'r').read())
+
+# this function converts the venue names into mermaid tags to give a bit of colour to the chart
+def venuemap(venue):
+    if venue == "Stage A":
+        mermaidtag = "crit, "
+    elif venue == "Stage B":
+        mermaidtag = "active, "
+    elif venue == "Stage C":
+        mermaidtag = ""
+    else:
+        mermaidtag = "done, "
+    return mermaidtag
+
+# I want to get the raw JSON into an ordered dictionary for plotting
+scheduledict = {}
+
+for index in range(len(emftalks)):
+    start_date = datetime.strptime(emftalks[index]["start_date"], "%Y-%m-%d %H:%M:%S")
+    duration = datetime.strptime(emftalks[index]["end_date"], "%Y-%m-%d %H:%M:%S") - start_date
+    scheduledict[start_date] = {"title": emftalks[index]["title"].split(":")[0], # colons mess up mermaid, plus the titles are too long to render nicely
+                                "venue": emftalks[index]["venue"],
+                                "speaker": emftalks[index]["speaker"],
+                                "description": emftalks[index]["description"],
+                                "duration": f"{duration.seconds/60:.0f}", # convert seconds into minutes
+                                "day": start_date.strftime("%A"), # we use the day to divide up the sections
+                                "daytime": start_date.strftime("%a %d, %H:%M")}
+
+
+with open("readme.md", "w") as fname:
+    fname.write("```mermaid\ngantt\n    title Your EMF Camp Schedule\n    dateFormat YYYY-MM-DD HH:mm\n") # mermaid preamble
+    sections = [] # we give each day its own section
+    for key in sorted(scheduledict):
+        if scheduledict[key]["day"] not in sections:
+            fname.write("    section "+scheduledict[key]["day"]+"\n")
+            sections.append(scheduledict[key]["day"]) # make sure we don't keep adding in the section line every time
+        # generate the mermaid string for each event
+        # indentation, title, optional venue colour/tag, start time, duration
+        fname.write("      "+scheduledict[key]["title"]+"  :"+venuemap(scheduledict[key]["venue"])+key.strftime("%Y-%m-%d %H:%M")+", "+scheduledict[key]["duration"]+"m\n")
+    fname.write("```")
+
+# magic date for the start of 2024 EMF Camp
+previouskey = datetime(2024, 5, 31, 10, 0)
+
+# print out a more informative schedule for you
+for key in sorted(scheduledict):
+    print("Time to next talk:", key - previouskey)
+    print("\n")
+    print(scheduledict[key]["title"])
+    print("by",scheduledict[key]["speaker"])
+    print(scheduledict[key]["venue"],"on",scheduledict[key]["daytime"],"for",scheduledict[key]["duration"],"mins")
+    print(scheduledict[key]["description"])
+    previouskey = key
---
+++ readme.md
@@ -0,0 +1,40 @@
+```mermaid
+gantt
+    title Your EMF Camp Schedule
+    dateFormat YYYY-MM-DD HH:mm
+    section Friday
+      Opening Ceremony  :crit, 2024-05-31 10:00, 30m
+      How to go solar off grid in the UK.   :2024-05-31 11:00, 30m
+      Small Town, Big Ideas - The challenges of building a rural Makerspace  :2024-05-31 11:40, 20m
+      Run your own fucking infrastructure - 2024 edition  :2024-05-31 12:10, 30m
+      Working Amateur Satellites on a Budget  :crit, 2024-05-31 13:50, 30m
+      The Best Word In Morse Code  :active, 2024-05-31 14:20, 20m
+      From Haunted Karaoke to the Humour Feature  :2024-05-31 14:40, 40m
+      The XZ backdoor - what, why, and how?  :crit, 2024-05-31 16:40, 30m
+      Engineering lessons from a 300 year-old poem    :active, 2024-05-31 16:50, 30m
+      The Auto Plane Spotter  :2024-05-31 18:10, 30m
+      Is everything difficult, or is it just me?  :crit, 2024-05-31 18:40, 40m
+    section Saturday
+      How to Save a Life  :2024-06-01 10:40, 30m
+      Gas boilers suck! Hack yours today, save money, save the planet!   :crit, 2024-06-01 11:20, 30m
+      OMG WTF SSO  :2024-06-01 11:50, 30m
+      introduction of lockpicking and the ethics of lockpicking  :done, 2024-06-01 12:00, 120m
+      Mathematical Origami  :done, 2024-06-01 12:30, 30m
+      How volunteers built and are now operating Hydro Power generation on the Thames  :crit, 2024-06-01 12:50, 30m
+      Hackers, Superheroes, and Super Massive Black Holes  :crit, 2024-06-01 13:30, 30m
+      Security Theatre  :active, 2024-06-01 16:10, 30m
+      The problem with medical influencers  :crit, 2024-06-01 16:30, 30m
+      A Brief History of Calendar Systems and Movable Feasts  :active, 2024-06-01 16:50, 30m
+      Data is plural  :2024-06-01 17:00, 20m
+      I Hacked Into My Own Car and other stories  :crit, 2024-06-01 17:50, 30m
+    section Sunday
+      I FOI'd every NHS board in England on autism assessment provision  :active, 2024-06-02 10:40, 20m
+      Making sense of London Underground's timetables and real-time data  :active, 2024-06-02 11:10, 30m
+      Titanium Spork Workshop  :done, 2024-06-02 12:00, 180m
+      An engineer’s guide to parenthood  :2024-06-02 12:20, 30m
+      Never Settle Down  :active, 2024-06-02 13:50, 30m
+      Steaming into Sustainability  :2024-06-02 14:50, 30m
+      Driving fancy LED panels with commodity hardware  :crit, 2024-06-02 15:00, 30m
+      Thomas the Privatised Tank Engine 30 years on  :crit, 2024-06-02 16:40, 30m
+      Infrastructure Review  :crit, 2024-06-02 18:00, 40m
+```
